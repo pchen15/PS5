96138f63ec37198e5beab46144b9f72ad795b1e1
fumblenator <jly29@cornell.edu>
2014-04-15 01:47:10 +0000

    updated remoteController.ml init

--- map_reduce/remoteController.ml
+++ map_reduce/remoteController.ml
@@ -1,7 +1,11 @@
 open Async.Std
 
 let init addrs =
-  failwith "Where you headed, cowboy?"
+  let aque = AQueue.create () in
+  let rec add aque lst = match lst with
+  	| [] -> ()
+  	| h::t -> (AQueue.push aque h); (add aque t) in
+  add aque addrs
 
 module Make (Job : MapReduce.Job) = struct
   let map_reduce inputs =
