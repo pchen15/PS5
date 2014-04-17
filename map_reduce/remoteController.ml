b1c7cffef7e8e301d339861c972f19a2faea16ca
pchen15 <pc482@cornell.edu>
2014-04-17 18:08:37 -0400

    added collect() and blank reduce() helper function

--- map_reduce/remoteController.ml
+++ map_reduce/remoteController.ml
@@ -14,8 +14,38 @@ module Make (Job : MapReduce.Job) = struct
 	  | [] -> ()
 	  | h::t -> AQueue.push workers h; add_to_queue t in
 	add_to_queue worker_list;
+	let rec assignJobs js = function
+      	  | (s,r,w)::t -> Writer.write_line w Job.name; assignJobs t
+          | [] -> () in
   	let rec maphelper lst =
   	  (* do something *)
   	  (* repeat for reduce *)
-
+  	  in
+  	let collect keyil = 
+	        let lcompare a b = 
+	          if fst(a) < fst(b) then -1
+	          else if fst(a) = fst(b) then 0
+	          else 1
+	        in
+	        let rec listify kyil acc = 
+	          match kyil with
+	            | (k,i)::t -> listify t ((k,[i])::acc)
+	            | [] -> acc
+	        in
+	        let rec combine lfied acc = 
+	          match lfied with
+	            | (k,il)::t -> (
+	              if acc = [] then combine t [(k,il)]
+	              else if k = fst(List.hd acc) then
+	                combine t ((k,(il @ (snd(List.hd acc))))::(List.tl acc))
+	              else combine t ((k,il)::acc)
+	            )
+	            | [] -> acc
+	        in combine (listify (List.sort lcompare keyil) []) [] in
+	let rec reducehelper lst = 
+	  (*do something similar to map*)
+	  in
+	(*need to establish worker connections first, not sure how*)
+	(maphelper inputs) >= (fun mres -> reducehelper (collect mres))
+	(*not sure what the entire map_reduce should return*)
 end
